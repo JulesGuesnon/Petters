a250bdf3aea41b3ca01d2c96353c7ac5e8a8eba7
Jules Guesnon <jules.guesnon@hetic.net>
2019-05-29 19:53:17 +0200

    :tada: Init files

---
+++ app/src/main/java/com/example/petters/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.petters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(fragmentContainer.id, MainFragment.newInstance())
+            .addToBackStack("init")
+            .commit()
+    }
+}
