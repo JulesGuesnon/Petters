6ea99de13744a26a619f8b7a7d24306a51f8ddc9
gross.yoan@gmail.com <32864409+grossyoan@users.noreply.github.com>
2019-06-03 01:31:06 +0200

    :sparkles: Login/Signup field error added :sparkles:

--- app/src/main/java/com/example/petters/MainActivity.kt
+++ app/src/main/java/com/example/petters/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager
             .beginTransaction()
-            .add(fragmentContainer.id, MainFragment.newInstance())
+            .add(fragmentContainer.id, SignupFragment.newInstance())
             .commit()
 
         FirebaseAuth.getInstance().signInWithEmailAndPassword("salut@les.amis", "testtest")
