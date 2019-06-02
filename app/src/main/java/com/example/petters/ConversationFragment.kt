71a74cd3c4c40d32a09283caa2f06e64e31696c0
Jules Guesnon <jules.guesnon@hetic.net>
2019-06-03 01:30:58 +0200

    :sparkles: Conversation input

--- app/src/main/java/com/example/petters/ConversationFragment.kt
+++ app/src/main/java/com/example/petters/ConversationFragment.kt
@@ -4,11 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class ConversationFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_conversation, container, false)
+        val view = inflater.inflate(R.layout.fragment_conversation, container, false)
+        val msgInput = view.findViewById<EditText>(R.id.conversation_input)
+        val btn = view.findViewById<ImageButton>(R.id.conversation_btn)
+
+        btn.setOnClickListener {
+            sendMessage(msgInput.text.toString())
+        }
+        return view
+    }
+
+    private fun sendMessage (msg: String) {
+        Toast.makeText(activity, msg, Toast.LENGTH_LONG).show()
     }
 
     companion object {
